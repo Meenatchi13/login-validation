8458c716213fed6513918e3025d5b603fc3f2146
Meenatchi13 <meenatchim80@gmail.com>
2026-07-13 20:31:18 +0530

    Login webpage

--- login.php
+++ login.php
@@ -3,17 +3,26 @@ $file="details.json";
 $jsondetails=file_get_contents($file);
 $data=json_decode($jsondetails,true);
 $email=$_POST["email"];
-
     if(str_ends_with($email,"@gmail.com"))
         {
-                $data[]=$email;
-                $jsondata=json_encode($data,JSON_PRETTY_PRINT);
-                file_put_contents($file,$jsondata);
+            if(in_array($email,$data))
+                {
+                    echo "Email already exits";
+                }
+                else
+                    {
+                     $data[]=$email;
+                     $jsondata=json_encode($data,JSON_PRETTY_PRINT);
+                     file_put_contents($file,$jsondata);
+                     echo" Email stored successfully";
+                    }
         }
+        
     else
         {
             echo"Not valid";
         }
+        
 
     
 ?>
